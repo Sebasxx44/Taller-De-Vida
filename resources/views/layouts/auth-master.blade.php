baaabc2959d10b8fb0d649c80a5c389c6676af47
Sebas Sanchez <sanchezz3s47@gmail.com>
2023-06-17 17:13:18 -0500

    New

--- resources/views/layouts/auth-master.blade.php
+++ resources/views/layouts/auth-master.blade.php
@@ -12,9 +12,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;800&display=swap" rel="stylesheet">
     <!-- cdn bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="https://tallerdevida-production.up.railway.app/assets/css/style.css">
+    <link rel="https://taller-de-vida-production.up.railway.app//assets/css/style.css">
     <!-- Logo de la aplicación-->
-    <link rel="shortcut icon" href="https://tallerdevida-production.up.railway.app/assets/images/logo.png">
+    <link rel="shortcut icon" href="https://tallerdevida-production.up.railway.app//assets/images/logo.png">
 
     <title>Fundacion</title>
 </head>
